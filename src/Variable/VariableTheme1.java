4b293e4b082a9369b07c50b4069b619016a71c01
jonnykea <jonny.kea@mail.ru>
2022-05-22 11:40:20 +1000

    Lesson_1 variable theme, task 1

---
+++ src/Variable/VariableTheme1.java
@@ -0,0 +1,19 @@
+package Variable;
+
+//Создание переменных и вывод их значений на консоль (это название задачи)
+public class VariableTheme1 {
+    public static void main(String[] args) {
+        byte a = -5;
+        short b = -5555;
+        int c = 555555;
+        long d = 5555555;
+        float e = 55555555;
+        double f = 55555555;
+        char g = 55;
+        boolean h = true;
+        System.out.println("simple variable: a, b, c, d, e, f = " +
+                "\n " + a + " " + b + " " + c + " " + d + " " + e + " " + f + " " +
+                "\n other simple variable g and h " + "\n " + g + " and " + h);
+
+    }
+}
