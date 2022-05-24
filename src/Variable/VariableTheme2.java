a55fdb07f4e886685800071f896cd80a53201d6b
jonnykea <Jonny.kea@mail.ru>
2022-05-24 16:08:25 +1000

    solutions for variable

---
+++ src/Variable/VariableTheme2.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class VariableTheme2 {
+    public static void main(String[] args) {
+
+        int x = 100; //стоимость первого товара
+        int y = 200;//стоимость второго товара
+        int z = ((x + y) / 100) * 11; //скидка на товары 11 %
+        int t = (x + y) - z; //общая стоимость товара с учетом скидки
+
+        System.out.println("сумма скидки - " + z + "\n"+"общая стоимость товара c учетом скидки - " + t);
+    }
+}
+
---
+++ src/Variable/VariableTheme4.java
@@ -0,0 +1,11 @@
+package Variable;
+
+public class VariableTheme4 {
+    public static void main(String[] args) {
+        byte a = 127;
+        short b = 32_767;
+        int c = 2_147_483_647;
+        long d = 9_223_372_036_854_775_807L;
+
+    }
+}
