f4333f38e56aa7df054926a614c372441d5206d3
jonnykea <Jonny.kea@mail.ru>
2022-05-24 16:11:00 +1000

    solutions for variable

---
+++ src/Variable/VariableTheme3.java
@@ -0,0 +1,14 @@
+package Variable;
+
+public class VariableTheme3 {
+    public static void main(String[] args) {
+        String a = "a";
+        String b = "v";
+        String c = "V";
+        String d = "J";
+        System.out.println(" " + d + "  " + a + " " + b + "  " + b + " " + a);
+        System.out.println(" " + d + "  " + a + " " + a + " " + b + "  " + b + " " + a + " " + a);
+        System.out.println(d + " " + d + "  " + a + a + a + a + "  " + c + " " + c + " " + a + a + a + a);
+        System.out.println(" " + d + d + "   " + a + "   " + a + " " + c + " " + a + "   " + a);
+    }
+}
