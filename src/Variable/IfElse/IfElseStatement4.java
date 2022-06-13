55fbff262ad267980eaf4b79c3be711c1683758b
Admin <jonny.kea@mail.ru>
2022-06-13 18:21:22 +1000

    IfElse solutions

---
+++ src/Variable/IfElse/IfElseStatement4.java
@@ -0,0 +1,29 @@
+package Variable.IfElse;
+//Поиск одинаковых цифр в числах
+//создайте две переменные, присвоив им два трехзначных целых числа
+//найдите в них одинаковые цифры, стоящие в одном и том же разряде
+//отобразите результат в формате:
+//в числах N и M одинаковые цифры A, B, C стоят в X, Y, Z разрядах
+public class IfElseStatement4 {
+    public static void main(String[] args) {
+        String N = "111";
+        char A = N.charAt(0);
+        char B = N.charAt(1);
+        char C = N.charAt(2);
+
+        String M = "111";
+        char X = M.charAt(0);
+        char Y = M.charAt(1);
+        char Z = M.charAt(2);
+        System.out.println("Число N " + N + "\n" + "Число M " + M);
+        if (A == X) {
+            System.out.println("В числах N и M одинаковые цифры A, B, C стоят в X, Y, Z в  первом разряде - " + A + " и " + X);
+        }
+        if (B == Y) {
+            System.out.println("В числах N и M одинаковые цифры A, B, C стоят в X, Y, Z во втором разряде - " + B + " и " + Y);
+        }
+        if (C == Z) {
+            System.out.println("В числах N и M одинаковые цифры A, B, C стоят в X, Y, Z в третьем разряде - " + C + " и " + Z);
+        }
+    }
+}
---
+++ src/Variable/IfElse/IfElseStatement6.java
@@ -0,0 +1,32 @@
+package Variable.IfElse;
+//Определение суммы вклада и начисленных банком %
+//сумма вклада равна 300_000 руб.
+//если она:
+//меньше 100 000, то банк начисляет 5% годовых
+//от 100 000 до 300 000, то — 7%
+//больше 300 000, то — 10%
+//отобразите в консоли:
+//сумму вклада
+//начисленный %
+//итоговую сумму с %
+public class IfElseStatement6 {
+    public static void main(String[] args) {
+        int sum = 300_000;
+
+        if (sum < 100_000) {
+            double interest = sum * 0.05;
+            System.out.println("Сумма вклада " + sum + "\n" + "Начисленный процент " + (int) interest +
+                    "\n" + "Итоговая сумма " + ((int) interest + sum));
+        } else if (sum < 300_000 && sum > 100_000) {
+            double interest = sum * 0.07;
+            System.out.println("Сумма вклада " + sum + "\n" + "Начисленный процент " + (int) interest +
+                    "\n" + "Итоговая сумма " + ((int) interest + sum));
+        } else if (sum >= 300_000) {
+            double interest = sum * 0.1;
+            System.out.println("Сумма вклада " + sum + "\n" + "Начисленный процент " + (int) interest +
+                    "\n" + "Итоговая сумма " + ((int) interest + sum));
+        }
+
+    }
+}
+
