4bb620cf0f43f30e051a4ac126f90ab35d3f336c
Admin <jonny.kea@mail.ru>
2022-07-27 18:29:51 +1000

    exercise with massive

--- src/com/startjava/lesson_2_3_4/array/ArrayTasks.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTasks.java
@@ -1,16 +1,28 @@
 package com.startjava.lesson_2_3_4.array;
 
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
 
 public class ArrayTasks {
     public static void main(String[] args) {
         System.out.println("Task 1 - Create new massive random with different elements and create length with console");
         Scanner lengthMassive = new Scanner(System.in);
-        System.out.println("\nwrite length of massive: ");
+        System.out.println("write length of massive: ");
         int length = lengthMassive.nextInt();
         int[] massive = new int[length];
+
+        fillMassive(massive);
+        print(massive);
+
+        sortBubble(massive);
+        print(massive);
+
+        searchMax(massive);
+        print(massive);
+    }
+
+    public static void fillMassive(int[] massive) {
+        System.out.println("New massive is created random, length - " + massive.length);
         int i = 0;
         while (i < massive.length - 1) {
             int secretNumber = (int) (Math.random() * 200) - 50;
@@ -26,26 +38,15 @@ public class ArrayTasks {
                 i++;
             }
         }
-        System.out.println("New massive is created random (-50 to 49) length - " + massive.length);
+    }
 
-        for (int num : massive) {
-            System.out.print(num + " ");
-        }
-        int max = 0;
-        i = 0;
-        while (i < massive.length - 1) {
-            if (max < Math.abs(massive[i])) {
-                max = Math.abs(massive[i]);
-            }
-            i++;
-        }
-        System.out.println("\nMax value of ABS in massive - " + max);
-        System.out.println("\nRealize the bubble sorting");
+    private static void sortBubble(int[] massive) {
+        System.out.println("\nRealize the bubble sorting by module");
         boolean isSort = false;
-        while(!isSort) {
+        while (!isSort) {
             isSort = true;
-            for (int j = 1; j < massive.length - 1; j++) {
-                if (Math.abs(massive[j]) < Math.abs(massive[j - 1])) {
+            for (int j = 1; j <= massive.length - 1; j++) {
+                if (Math.abs(massive[j - 1]) > Math.abs(massive[j])) {
                     int tmp = massive[j - 1];
                     massive[j - 1] = massive[j];
                     massive[j] = tmp;
@@ -53,9 +54,24 @@ public class ArrayTasks {
                 }
             }
         }
+    }
+
+    private static void print(int[] massive) {
         for (int num : massive) {
             System.out.print(num + " ");
         }
+    }
+
+    private static void searchMax(int[] massive) {
+        int max = 0;
+        int i = 0;
+        while (i <= massive.length - 1) {
+            if (max < Math.abs(massive[i])) {
+                max = Math.abs(massive[i]);
+            }
+            i++;
+        }
+        System.out.println("\nMax value by module in massive - " + max);
     }
 }
 
--- src/com/startjava/lesson_2_3_4/array/ExperimentList.java
+++ src/com/startjava/lesson_2_3_4/array/ExperimentList.java
@@ -6,17 +6,25 @@ public class ExperimentList {
     public static void main(String[] args) {
         List<Integer> list = new java.util.ArrayList<>();
         list.add(56);
+
+        fillList(list);
+        printArray(list);
+
+        list.set(4, 5);
+        System.out.println("");
+
+        printArray(list);
+    }
+    private static void fillList(List<Integer> list) {
         for (int i = 0; i < 10; i++) {
             int number = (int) (Math.random() * 10);
             list.add(number);
         }
+    }
+
+    private static void printArray(List<Integer> list){
         for (int num : list) {
             System.out.print(num + " ");
-        }
-        list.set(4, 5);
-        System.out.println("");
-        for (int num : list) {
-            System.out.print(+ num + " ");
         }
     }
 }
