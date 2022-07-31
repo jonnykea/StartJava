f1ef991f84230cf19a52877101f0491c121d0033
jonnykea <jonny.kea@mail.ru>
2022-07-31 14:06:37 +1000

    correct tasks for arrays

--- src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
+++ src/com/startjava/lesson_2_3_4/array/ArrayTheme.java
@@ -7,49 +7,54 @@ public class ArrayTheme {
         System.out.println("Original array");
         print(intArray);
         System.out.println("\nReverse array");
-        sortBubbleReverse(intArray);
+        int temp = 0;
+        for (int i = 0; i < intArray.length / 2; i++) {
+            temp = intArray[intArray.length - i - 1];
+            intArray[intArray.length - i - 1] = intArray[i];
+            intArray[i] = temp;
+        }
         print(intArray);
 
         System.out.println("\n\nTask 2 - Display of array product  [0,9] except 0,9");
-        intArray = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int length = 10;
+        intArray = new int[length];
+        for (int i = 0; i < length; i++) {
+            intArray[i] = i;
+        }
         int product = 1;
-        for (int i = 1; i < intArray.length; i++) {
-            product *= (i == intArray.length - 1) ? 1 : intArray[i];
-            if (i == intArray.length - 2) {
-                System.out.print(intArray[i] + " = ");
-            } else {
-                System.out.print(intArray[i] + " * ");
-            }
+        for (int i = 1; i < length - 1; i++) {
+            product *= intArray[i];
+            System.out.print(i == length - 2 ? (intArray[i] + " = ") : (intArray[i] + " * "));
         }
         System.out.print(product);
 
         System.out.println("\n\nTask 3 - Remove elements from array  [0,1]");
-        double[] doubleArray = new double[15];
-        fillMassiveD(doubleArray, 1, 0);
+        length = 15;
+        double[] doubleArray = new double[length];
+        for (int i = 0; i < length; i++) {
+            doubleArray[i] = Math.random();
+        }
         System.out.println("Original array");
         int counter = 8;
         for (double num : doubleArray) {
             System.out.printf("%.2f ", num);
             counter--;
             if (counter == 0) {
-                System.out.println("");
-                counter = 8;
+                System.out.println();
             }
         }
-        for (int i = 0; i <= doubleArray.length - 1; i++) {
-            if (doubleArray[i] > doubleArray[6]) {
+        for (int i = 0; i < length; i++) {
+            if (doubleArray[i] > doubleArray[(length / 2)]) {
                 doubleArray[i] = 0;
             }
         }
-        System.out.println("");
-        System.out.println("Changed array");
+        System.out.println("\nChanged array");
         counter = 7;
         for (double num : doubleArray) {
             System.out.printf("%.2f ", num);
             counter--;
             if (counter == 0) {
-                System.out.println("");
-                counter = 7;
+                System.out.println();
             }
         }
 
@@ -64,7 +69,7 @@ public class ArrayTheme {
             for (int j = 14; j >= i; j--) {
                 System.out.print(arrayChar[j] + " ");
             }
-            System.out.println("");
+            System.out.println();
         }
 
         System.out.println("\nTask 5 - Generate unique elements [60,100]");
@@ -76,7 +81,7 @@ public class ArrayTheme {
             System.out.printf("%3d ", num);
             counter--;
             if (counter == 0) {
-                System.out.println("");
+                System.out.println();
                 counter = 10;
             }
         }
@@ -130,15 +135,6 @@ public class ArrayTheme {
         }
     }
 
-    public static void fillMassiveD(double[] massive, int topLimit, int lowLimit) {
-        System.out.println("New massive is created random, length - " + massive.length);
-        int max = topLimit - lowLimit;
-        for (int i = 0; i < massive.length; i++) {
-            double randomNum = lowLimit + (Math.random() * (max));
-            massive[i] = randomNum;
-        }
-    }
-
     private static void sortBubble(int[] array) {
         boolean isSort = false;
         while (!isSort) {
@@ -154,30 +150,9 @@ public class ArrayTheme {
         }
     }
 
-    private static void sortBubbleReverse(int[] array) {
-        boolean isSort = false;
-        while (!isSort) {
-            isSort = true;
-            for (int j = 1; j < array.length; j++) {
-                if (Math.abs(array[j - 1]) < Math.abs(array[j])) {
-                    int tmp = array[j - 1];
-                    array[j - 1] = array[j];
-                    array[j] = tmp;
-                    isSort = false;
-                }
-            }
-        }
-    }
-
     private static void print(int[] array) {
         for (int num : array) {
             System.out.print(num + " ");
-        }
-    }
-
-    private static void printD(double[] array) {
-        for (double num : array) {
-            System.out.printf(" %.3f ", num);
         }
     }
 }
