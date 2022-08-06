8c977d0fe975bbd4f72654aa73eb3698574c2268
jonnykea <jonny.kea@mail.ru>
2022-08-06 18:01:25 +1000

    fulfilment the total homework

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,8 +1,14 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import static java.lang.Integer.parseInt;
+
 public class Calculator {
-    public void calculate(int a, int b, String sign) {
-        int result;
+    public Integer calculate(String expression) {
+        String[] arrayMath = expression.split(" ");
+        int a = parseInt(arrayMath[0]);
+        String sign = arrayMath[1];
+        int b = parseInt(arrayMath[2]);
+        Integer result = null;
         switch (sign) {
             case "+":
                 result = a + b;
@@ -17,16 +23,15 @@ public class Calculator {
                 result = a / b;
                 break;
             case "^":
-                result = (int)Math.pow(a,b);
+                result = (int) Math.pow(a, b);
                 break;
             case "%":
                 result = a % b;
                 break;
             default:
-                System.out.println("некорректное действие " + sign + ", доступные мат. действия: + - * / ^ %");
-                return;
+                System.out.println("некорректное действие - " + sign + "  доступные мат. действия: + - * / ^ %");
         }
-        System.out.println(a + " " + sign + " " + b + " = " + result);
+        return result;
     }
 }
 
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -2,28 +2,25 @@ package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
 
-import static java.lang.Integer.parseInt;
-
 public class CalculatorTest {
     static Scanner console = new Scanner(System.in);
-    static String answer;
 
     public static void main(String[] args) {
         do {
             System.out.print("Введите математическое выражение, например 5 * 10 " +
-                    "\nдоступные мат. действия: + - * / ^ %: ");
+                    "\nдоступные мат. действия: + - * / ^ %: \n");
             String expression = console.nextLine();
-            String[] arrayMath = expression.split(" ");
-            int a = parseInt(arrayMath[0]);
-            String sign = arrayMath[1];
-            int b = parseInt(arrayMath[2]);
             Calculator calculator = new Calculator();
-            calculator.calculate(a, b, sign);
+            Integer result = calculator.calculate(expression);
+            if (result != null) {
+                System.out.print(result);
+            }
         } while (isNext());
     }
 
     private static boolean isNext() {
-        System.out.print("Хотите сыграть еще раз? Введите yes или no... ");
+        String answer;
+        System.out.println("\nХотите сыграть еще раз? Введите yes или no... ");
         answer = console.nextLine().toLowerCase();
         while (!answer.equals("no")) {
             if (answer.equals("yes")) {
