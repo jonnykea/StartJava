b793423b76b95f0cad32072899379fc965aec93f
jonnykea <jonny.kea@mail.ru>
2023-06-02 14:27:23 +1000

    tasks SQL

--- src/tasks/tasksExam.sql
+++ src/tasks/tasksExam.sql
@@ -3,8 +3,10 @@ DROP TABLE IF EXISTS Employee;
 CREATE TABLE Employee
 (
     id        SERIAL PRIMARY KEY,
-    name      CHAR(36) NOT NULL,
-    salary    int      NOT NULL,
+    name      CHAR(36) unique,
+    salary    int
+        constraint emp_salary
+            CHECK (salary between 60000 and 90000),
     managerId INTEGER default null
 );
 
@@ -12,14 +14,27 @@ INSERT INTO Employee(id, name, salary, managerId)
 values (1, 'JOE', '70000', 3),
        (2, 'HENRY', '80000', 4),
        (3, 'SAM', '60000', Null),
-       (4, 'MAX', '90000',Null);
+       (4, 'MAX', '90000', Null);
 
-Create table If Not Exists Employee (id int, name varchar(255), salary int, managerId int);
+INSERT INTO Employee(id, name, salary, managerId)
+values (5, 'JOE', '5000', null);
+
+Create table If Not Exists Employee
+(
+    id        int,
+    name      varchar(255),
+    salary    int,
+    managerId int
+);
 Truncate table Employee;
-insert into Employee (id, name, salary, managerId) values ('1', 'Joe', '70000', '3');
-insert into Employee (id, name, salary, managerId) values ('2', 'Henry', '80000', '4');
-insert into Employee (id, name, salary, managerId) values ('3', 'Sam', '60000', Null);
-insert into Employee (id, name, salary, managerId) values ('4', 'Max', '90000', Null);
+insert into Employee (id, name, salary, managerId)
+values ('1', 'Joe', '70000', '3');
+insert into Employee (id, name, salary, managerId)
+values ('2', 'Henry', '80000', '4');
+insert into Employee (id, name, salary, managerId)
+values ('3', 'Sam', '60000', Null);
+insert into Employee (id, name, salary, managerId)
+values ('4', 'Max', '90000', Null);
 
 SELECT E.name
 FROM EMPLOYEE E
@@ -32,22 +47,28 @@ WHERE E.SALARY > M.SALARY;
 
 SELECT *
 FROM EMPLOYEE E
-        inner JOIN EMPLOYEE M ON E.managerId = M.id;
+         inner JOIN EMPLOYEE M ON E.managerId = M.id;
 
-SELECT * From Employee e
+SELECT *
+From Employee e
 where e.name ilike 'j%';
 
-SELECT * From Employee e
+SELECT *
+From Employee e
 where e.name like 'J%';
 
-SELECT * From Employee e
+SELECT *
+From Employee e
 where e.name like '__m%';
 
-SELECT * From Employee e
+SELECT *
+From Employee e
 where e.name ilike '____y%';
 
-SELECT * From Employee e
+SELECT *
+From Employee e
 where e.name like 'J%'
 union all
-SELECT * From Employee e
+SELECT *
+From Employee e
 where e.name = 'HENRY';
